49982cbc383214d8eef5f097a26576275d78052f
Jorge González <jorgon183@hotmail.com>
2021-10-18 20:06:01 -0300

    pruebas de sonar

--- pipeline.groovy
+++ pipeline.groovy
@@ -62,21 +62,6 @@ pipeline {
                 }
             }           
         }
-
-        stage("build & SonarQube analysis") {
-            steps {
-                withSonarQubeEnv('My SonarQube Server') {
-                sh 'mvn clean package sonar:sonar'
-                }
-            }
-        }
-        stage("Quality Gate") {
-        steps {
-            timeout(time: 1, unit: 'HOURS') {
-            waitForQualityGate abortPipeline: true
-            }
-            }
-        }
     }
 
 }
